215f1953026a1675418f4195343e65169fe9c067
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2025-01-22 13:21:49 +0600

    content section created

---
+++ src/components/global/Section.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'content-section',
+  props: {
+    theme: {
+      type: String,
+      default: 'bg-gray-100',
+    },
+  },
+}
+</script>
+
+<template>
+  <section class="block px-6 md:px-14 lg:px-20 xl:px-28 py-6 md:py-14 lg:py-20 xl:py-28" :class="theme" aria-label="Content Section" >
+    <div class="container" >
+      <slot />
+    </div>
+  </section>
+</template>
--- src/components/home/Recipe.vue
+++ src/components/home/Recipe.vue
@@ -1,4 +1,5 @@
 <script>
+import Section from '@/components/global/Section.vue'
 import api from '@/api.js';
 
 export default {
@@ -9,13 +10,14 @@ export default {
       loading: true,
     }
   },
+  components: { Section },
   created() {
     this.fetchRecipes();
   },
   methods: {
     async fetchRecipes() {
       try {
-        const response = await api.get(`/recipes?limit=9&sortBy=id&order=desc&select=id,name,image,cuisine,rating`);
+        const response = await api.get(`/recipes?limit=6&sortBy=id&order=desc&select=id,name,image,cuisine,rating`);
         this.recipes = response.data.recipes;
       } catch (error) {
         console.error('Error fetching recipes:', error);
@@ -28,7 +30,19 @@ export default {
 </script>
 
 <template>
-  <section class="block px-28 py-28" >
+  <Section v-if="loading" theme="bg-gray-100" >
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10" >
+      <div v-for="recipe in recipes" class="block border border-teal-300 rounded shadow-lg p-4 animate-pulse" :key="recipe.id" >
+        <div class="block bg-teal-300 rounded w-full h-80" ></div>
+        <div class="block bg-teal-300 rounded-full mt-4 w-full h-6" ></div>
+        <div class="flex justify-between items-center" >
+          <div class="inline-block bg-teal-300 rounded-full my-4 w-32 h-4" ></div>
+          <div class="inline-block bg-teal-300 rounded-full my-4 w-32 h-4" ></div>
+        </div>
+      </div>
+    </div>
+  </Section>
+  <Section v-else theme="bg-gray-100" >
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10" >
       <div v-for="recipe in recipes" class="block border border-teal-300 rounded shadow-lg p-4" :key="recipe.id" data-aos="fade-up" :data-aos-delay="recipe.id * 100" data-aos-duration="1000" >
         <div>
@@ -43,5 +57,5 @@ export default {
         </div>
       </div>
     </div>
-  </section>
+  </Section>
 </template>

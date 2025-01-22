6ee161d44a40d23c658ecde7c7296394070da9db
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2025-01-22 11:49:26 +0600

    home page created

---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<script>
+import Main from '../layouts/Main.vue';
+
+export default {
+  name: 'home-page',
+  components: { Main }
+}
+</script>
+
+<template>
+  <Main>
+    <h1>This is Homepage</h1>
+  </Main>
+</template>
